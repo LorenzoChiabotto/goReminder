b7007d291f049973cc379fef15910cb617ae6d5f
Lorenzo Chiabotto <lorenzochiabotto@gmail.com>
2020-11-18 00:47:06 -0300

    jwt token generated for 1 hour and validated

--- authentication/authentication.go
+++ authentication/authentication.go
@@ -3,8 +3,12 @@ package authentication
 import (
 	"fmt"
 	"log"
+	"os"
+	"strings"
+	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
 )
 
 type MyCustomClaims struct {
@@ -13,28 +17,48 @@ type MyCustomClaims struct {
 }
 
 func GenerateToken() (string, error) {
+	err := godotenv.Load("./.env")
+	if err != nil {
+		return "", fmt.Errorf("Error loading .env file: %v", err)
+	}
+
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyCustomClaims{
 		"thisIsAnUserTest",
 		jwt.StandardClaims{
-			ExpiresAt: 3600,
-			Issuer:    "app",
+			ExpiresAt: time.Now().Add(time.Minute * 60).Unix(),
+			Issuer:    "endpointValidateGo",
 		},
 	})
 
-	mySecret := "summerReminder"
-	signedToken, err := token.SignedString([]byte(mySecret))
-
+	signedToken, err := token.SignedString([]byte(os.Getenv("secret")))
+	if err != nil {
+		return "", fmt.Errorf("Error signing the token: %v", err)
+	}
 	return signedToken, err
 }
 
-func ValidateToken(receivedToken string) (bool, error) {
-	_, err := jwt.Parse(receivedToken, func(token *jwt.Token) (interface{}, error) {
-		mySecret, ok := token.Method.(*jwt.SigningMethodHMAC)
+func ValidateToken(receivedToken string) error {
+	err := godotenv.Load("./.env")
+	splittedToken := strings.Split(receivedToken, " ")
+	if len(splittedToken) != 2 {
+		return fmt.Errorf("Invalid token structure: %v", receivedToken)
+	}
+
+	token, err := jwt.Parse(splittedToken[1], func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 		if !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
-		log.Println(mySecret)
-		return mySecret, nil
+
+		return []byte(os.Getenv("secret")), nil
 	})
-	return false, err
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		log.Println(claims)
+	}
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 	"./authentication"
 )
 
+type Token struct {
+	token string
+}
+
 func addReminder(w http.ResponseWriter, r *http.Request) {
 	var placeholder string
 	err := json.NewDecoder(r.Body).Decode(&placeholder)
@@ -51,19 +55,27 @@ func deleteReminder(w http.ResponseWriter, r *http.Request) {
 
 }
 func getReminders(w http.ResponseWriter, r *http.Request) {
-	var placeholder string
-	err := json.NewDecoder(r.Body).Decode(&placeholder)
+	var placeholder Token
+
+	err := authentication.ValidateToken(r.Header.Get("Authorization"))
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		log.Println(err)
+		w.WriteHeader(http.StatusForbidden)
 		return
 	}
+
+	//err = json.NewDecoder(r.Body).Decode(&placeholder)
+	//if err != nil {
+	//	w.WriteHeader(http.StatusBadRequest)
+	//	return
+	//}
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(placeholder)
 }
 
-func addUser(w http.ResponseWriter, r *http.Request) {
+func getUserToken(w http.ResponseWriter, r *http.Request) {
 	token, err := authentication.GenerateToken()
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
@@ -75,7 +87,7 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(token)
 }
-func getUserToken(w http.ResponseWriter, r *http.Request) {
+func addUser(w http.ResponseWriter, r *http.Request) {
 	var token string
 	err := json.NewDecoder(r.Body).Decode(&token)
 	if err != nil {
@@ -84,11 +96,11 @@ func getUserToken(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	token2, err := authentication.ValidateToken(token)
+	//err := authentication.ValidateToken(token)
 
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(token2)
+	//json.NewEncoder(w).Encode(token2)
 }
 func getUser(w http.ResponseWriter, r *http.Request) {}
 
@@ -104,10 +116,10 @@ func main() {
 	r.HandleFunc("/users", getUser).Methods("GET")
 	r.HandleFunc("/users", addUser).Methods("POST")
 
-	r.HandleFunc("/{userName}/reminders", addReminder).Methods("POST")
-	r.HandleFunc("/{userName}/reminders", editReminder).Methods("PUT")
-	r.HandleFunc("/{userName}/reminders", deleteReminder).Methods("POST")
-	r.HandleFunc("/{userName}/reminders", getReminders).Methods("GET")
+	r.HandleFunc("/reminders", addReminder).Methods("POST")
+	r.HandleFunc("/reminders", editReminder).Methods("PUT")
+	//	r.HandleFunc("/reminders", deleteReminder).Methods("POST")
+	r.HandleFunc("/reminders", getReminders).Methods("GET")
 
 	http.ListenAndServe(":80", r)
 }
